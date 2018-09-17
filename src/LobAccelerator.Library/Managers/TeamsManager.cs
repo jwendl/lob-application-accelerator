17e4487f8d5457b9b23005eb7f9a65dbd60992bb
Allan Targino <allan_targino@hotmail.com>
2018-09-17 18:04:09 -0300

    adding http request abstraction for posting content

--- src/LobAccelerator.Library/Managers/TeamsManager.cs
+++ src/LobAccelerator.Library/Managers/TeamsManager.cs
@@ -1,8 +1,12 @@
-﻿using LobAccelerator.Library.Interfaces;
+﻿using LobAccelerator.Library.Extensions;
+using LobAccelerator.Library.Interfaces;
 using LobAccelerator.Library.Models;
 using LobAccelerator.Library.Models.Teams;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,9 +14,41 @@ namespace LobAccelerator.Library.Managers
 {
     public class TeamsManager : ITeamsManager
     {
+        private readonly HttpClient httpClient;
+
+        public TeamsManager(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
         public Task CreateResourceAsync(Team resource)
         {
             throw new NotImplementedException();
+        }
+
+        public async Task AddPeopleToChannelAsync(IEnumerable<string> members, string teamId)
+        {
+            var addMemberUrl = $"beta/groups/{teamId}/members/$ref";
+
+            foreach (var member in members)
+            {
+                var channelObj = new CreateChannelGraphObject(member);
+                await httpClient.PostContentAsync(addMemberUrl, channelObj);
+            }
+        }
+
+        private class CreateChannelGraphObject
+        {
+            private readonly string memberId;
+
+            [JsonProperty("@odata.id")]
+            public string DisplayName
+                => $"https://graph.microsoft.com/beta/directoryObjects/{memberId}";
+
+            public CreateChannelGraphObject(string memberId)
+            {
+                this.memberId = memberId;
+            }
         }
     }
 }
--- src/LobAccelerator.Library/Managers/WorkflowManager.cs
+++ src/LobAccelerator.Library/Managers/WorkflowManager.cs
@@ -2,17 +2,30 @@
 using LobAccelerator.Library.Managers;
 using LobAccelerator.Library.Models;
 using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace LobAccelerator.Manager.Library
 {
     public class WorkflowManager : IWorkflowManager
     {
+        private const string BASE_URL = "https://graph.microsoft.com/";
         private readonly ITeamsManager teamsManager;
+        private readonly HttpClient httpClient;
 
-        public WorkflowManager()
+        public WorkflowManager(string accessToken)
         {
-            teamsManager = new TeamsManager();
+            httpClient = CreateHttpClient(BASE_URL, accessToken);
+
+            teamsManager = new TeamsManager(httpClient);
+        }
+
+        private HttpClient CreateHttpClient(string baseUrl, string accessToken)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(baseUrl);
+            client.DefaultRequestHeaders.Add("Authorization", $"bearer {accessToken}");
+            return client;
         }
 
         public async Task CreateResourceAsync(Workflow resource)
