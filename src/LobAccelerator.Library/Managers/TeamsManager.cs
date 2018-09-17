cd5db8f529dcc1ddef7b8a58efa09c39f7095488
Allan Targino <allan_targino@hotmail.com>
2018-09-17 16:13:33 -0300

    fixing TeamsManagers

--- src/LobAccelerator.Library/Managers/TeamsManager.cs
+++ src/LobAccelerator.Library/Managers/TeamsManager.cs
@@ -1,11 +1,17 @@
 ﻿using LobAccelerator.Library.Interfaces;
+using LobAccelerator.Library.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace LobAccelerator.Library.Managers
 {
     public class TeamsManager : ITeamsManager
     {
+        public Task CreateResourceAsync(Team resource)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
