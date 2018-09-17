ad36d2a379e96512ecde5d945cb71019255d0d9b
Allan Targino <allan_targino@hotmail.com>
2018-09-17 18:04:09 -0300

    adding http request abstraction for posting content

---
+++ src/LobAccelerator.Library/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LobAccelerator.Library.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<HttpResponseMessage> PostContentAsync
+            (this HttpClient httpClient, string url, object content)
+        {
+            var objectStr = JsonConvert.SerializeObject(content);
+            var body = new StringContent(objectStr, Encoding.ASCII, "application/json");
+
+            return await httpClient.PostAsync(url, body);
+        }
+    }
+}
