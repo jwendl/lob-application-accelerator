af193b2ba0af1e03eb1ec65837641f59550bc748
Fernando de Oliveira <fedeoliv@microsoft.com>
2018-09-17 21:09:10 +0000

    Teams interface methods
    Teams methods added to interface (create teams, add people to teams channel)

--- src/LobAccelerator.Library/Interfaces/ITeamsManager.cs
+++ src/LobAccelerator.Library/Interfaces/ITeamsManager.cs
@@ -1,12 +1,12 @@
-﻿using LobAccelerator.Library.Models;
-using LobAccelerator.Library.Models.Teams;
-using System;
+﻿using LobAccelerator.Library.Models.Teams;
 using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
 
 namespace LobAccelerator.Library.Interfaces
 {
     public interface ITeamsManager : IResourceManager<Team>
     {
+        Task CreateResourceAsync(Team resource);
+        Task AddPeopleToChannelAsync(IEnumerable<string> members, string teamId);
     }
 }
