7ac5b8b92720be298fda2c7f9ab12817be0d4fd0
Fernando de Oliveira <fedeoliv@microsoft.com>
2018-09-17 21:56:28 +0000

    Teams groups HTTP request
    Creation of CreateGroup method

---
+++ src/LobAccelerator.Library/Models/Teams/Groups/GroupContent.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace LobAccelerator.Library.Models.Teams.Groups
+{
+    [JsonObject("groupContent")]
+    public class GroupContent
+    {
+        [JsonProperty("description")]
+        [Description("Group description")]
+        public string Description { get; set; }
+
+        [JsonProperty("displayName")]
+        [Description("The name to display in the address book for the group.")]
+        public string DisplayName { get; set; }
+
+        [JsonProperty("groupTypes")]
+        [Description("Office 365 or dynamic group.")]
+        public string[] GroupTypes { get; set; }
+        
+        [JsonProperty("mailEnabled")]
+        [Description("Set this to true if creating an Office 365 Group or false if creating dynamic or security group.")]
+        public bool MailEnabled { get; set; }
+
+        [JsonProperty("mailNickname")]
+        [Description("Set to true for security-enabled groups or if creating a dynamic or security group. False if creating an Office 365 Group.")]
+        public string MailNickname { get; set; }
+
+        [JsonProperty("securityEnabled")]
+        [Description("True for security-enabled groups or if creating a dynamic or security group. False if creating an Office 365 Group.")]
+        public bool SecurityEnabled { get; set; }
+
+        [JsonProperty("owners@odata.bind")]
+        [Description("This property represents the owners for the group at creation time.")]
+        public List<string> Owners { get; set; }
+
+        [JsonProperty("members@odata.bind")]
+        [Description("This property represents the members for the group at creation time.")]
+        public IEnumerable<string> Members { get; set; }
+    }
+}
