385af304c397a71d506b6f530c43ff891f3042e3
Allan Targino <allan_targino@hotmail.com>
2018-09-18 15:22:58 -0300

    adding validation tests

--- src/LobAccelerator.Library.Tests/TokenTests.cs
+++ src/LobAccelerator.Library.Tests/TokenTests.cs
@@ -1,8 +1,10 @@
 ﻿using LobAccelerator.Library.Tests.Utils.Auth;
 using LobAccelerator.Library.Tests.Utils.Configuration;
+using LobAccelerator.Library.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -33,6 +35,25 @@ namespace LobAccelerator.Library.Tests
             Assert.NotEmpty(returnedScopes);
             Assert.True(returnedScopes.Intersect(scopes).Count() == scopes.Count());
             Assert.Equal(configuration["AzureAd:Resource"], token.resource);
+        }
+
+        [Fact]
+        public async Task ValidateAccessToken()
+        {
+            //Arrange
+            var configuration = new ConfigurationManager();
+            var tokenRetriever = new TokenRetriever(configuration);
+            var scopes = new string[] { "Group.ReadWrite.All" };
+            var expectedAudience = configuration["AzureAd:ClientId"];
+            var expectedIssuer = configuration["AzureAd:TenantId"];
+
+            //Act
+            var token = await tokenRetriever.GetTokenByAuthorizationCodeFlowAsync(scopes);
+            var header = new AuthenticationHeaderValue("Bearer", token.access_token);
+            var validation = await AuthHelper.ValidateTokenAsync(header, expectedIssuer, expectedAudience, scopes);
+
+            //Assert
+            Assert.NotNull(validation);
         }
     }
 }
