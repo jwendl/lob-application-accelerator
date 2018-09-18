8a10a6e9c539538dc9f55b4d1b62849ea1909e7e
tessmichi <tess.distefano@gmail.com>
2018-09-18 16:53:25 -0700

    start tests

--- src/LobAccelerator.Library.Tests/StorageTests.cs
+++ src/LobAccelerator.Library.Tests/StorageTests.cs
@@ -2,34 +2,36 @@
 using System.Collections.Generic;
 using System.Text;
 using LobAccelerator.Library.Tests.Utils.Configuration;
+using LobAccelerator.Library.Utils;
 
 namespace LobAccelerator.Library.Tests
 {
     class StorageTests
     {
         private readonly ConfigurationManager configuration;
+        private readonly string connectionString = "UseDevelopmentStorage=true;";
         private readonly string containerName = "TokenCacheContainer";
         private readonly string blobName = "TokenCacheBlob";
 
-        public TeamsTests()
+        public StorageTests()
         {
             configuration = new ConfigurationManager();
         }
         
         [Fact]
-        public async Task BlobExistsFalse()
+        public async Task UploadBytesToBlob()
         {
             //Arrange
-            var team = Workflow.Teams.First();
-            HttpClient httpClient = await GetHttpClient();
-            var teamsManager = new TeamsManager(httpClient);
+            var bytes = new byte[5];
+            Random random = new Random();
+            random.NextBytes(bytes);
 
             //Act
-            var result = await teamsManager.CreateGroupAsync(team);
+            var result = await StorageHelper.UploadBlobAsync(connectionString, containerName, blobName, bytes);
 
             //Assert
+            Assert.True(result);
             Assert.False(result.HasError);
         }
-
     }
 }
