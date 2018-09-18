ede3242c954f3700a83319370836cb788ded3805
tessmichi <tess.distefano@gmail.com>
2018-09-18 16:37:26 -0700

    move connection string our of storage helper

---
+++ src/LobAccelerator.Library.Tests/StorageTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LobAccelerator.Library.Tests.Utils.Configuration;
+
+namespace LobAccelerator.Library.Tests
+{
+    class StorageTests
+    {
+        private readonly ConfigurationManager configuration;
+        private readonly string containerName = "TokenCacheContainer";
+        private readonly string blobName = "TokenCacheBlob";
+
+        public TeamsTests()
+        {
+            configuration = new ConfigurationManager();
+        }
+        
+        [Fact]
+        public async Task BlobExistsFalse()
+        {
+            //Arrange
+            var team = Workflow.Teams.First();
+            HttpClient httpClient = await GetHttpClient();
+            var teamsManager = new TeamsManager(httpClient);
+
+            //Act
+            var result = await teamsManager.CreateGroupAsync(team);
+
+            //Assert
+            Assert.False(result.HasError);
+        }
+
+    }
+}
