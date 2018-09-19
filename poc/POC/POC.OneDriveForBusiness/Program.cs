e3f0074375a24ad28e0797fb6229a55fecedca56
Allan Targino <allan_targino@hotmail.com>
2018-09-19 20:04:19 -0300

    creating service for onedrive for business

--- poc/POC/POC.OneDriveForBusiness/Program.cs
+++ poc/POC/POC.OneDriveForBusiness/Program.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
-using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -12,29 +9,36 @@ namespace POC.OneDriveForBusiness
     {
         static async Task Main(string[] args)
         {
-            string resource = "https://graph.microsoft.com";
+            // Input
             string clientId = "398917db-d35d-4bd9-81cf-c3ff85c60e12";
-            string onedriveFolder = "TransferFiles/MyFolder1/MyFolder2";
+            string teamId = "9264c179-13ad-4208-bd9c-92bd91430b3d";
 
-            string accessToken = await GetTokenViaCode(resource, clientId);
+            // Use cases:
+            string onedriveFolder = "TransferFiles/MyFolder1/MyFolder3";
+            string onedriveFile = "TransferFiles/MyFolder1/MyFolder2/Onboarding.docx";
+            string onedriveFileRoot = "Book.xlsx";
 
-            HttpClient httpClient = CreateHttpClient(accessToken);
+            string accessToken = await GetTokenViaCode(clientId);
+            var httpClient = CreateHttpClient(accessToken);
+            var oneDriveManager = new OneDriveManager(httpClient);
 
-            var folderId = await GetFolderId(httpClient, onedriveFolder);
-            Console.WriteLine(folderId);
+            await oneDriveManager.CopyFolderFromOneDriveToTeams(teamId, onedriveFolder);
+            await oneDriveManager.CopyFileFromOneDriveToTeams(teamId, onedriveFile);
+            await oneDriveManager.CopyFileFromOneDriveToTeams(teamId, onedriveFileRoot);
 
             Console.ReadLine();
         }
 
-        static HttpClient CreateHttpClient(string accessToken)
+        private static HttpClient CreateHttpClient(string accessToken)
         {
             var httpClient = new HttpClient();
             httpClient.DefaultRequestHeaders.Add("Authorization", $"bearer {accessToken}");
             return httpClient;
         }
 
-        static async Task<string> GetTokenViaCode(string resource, string clientId)
+        private static async Task<string> GetTokenViaCode(string clientId)
         {
+            var resource = "https://graph.microsoft.com";
             var ctx = new AuthenticationContext("https://login.microsoftonline.com/common");
 
             DeviceCodeResult codeResult = await ctx.AcquireDeviceCodeAsync(resource, clientId);
@@ -42,31 +46,8 @@ namespace POC.OneDriveForBusiness
             Console.WriteLine("Message: " + codeResult.Message);
 
             var token = await ctx.AcquireTokenByDeviceCodeAsync(codeResult);
+
             return token.AccessToken;
         }
-
-        static async Task<string> GetFolderId(HttpClient httpClient, string onedriveFolder)
-        {
-            string url = $"https://graph.microsoft.com/v1.0/me/drive/root:/{onedriveFolder}:?$select=id";
-
-            var response = await httpClient.GetAsync(url);
-            response.EnsureSuccessStatusCode();
-
-            var responseStr = await response.Content.ReadAsStringAsync();
-            return JObject.Parse(responseStr)["id"].Value<string>();
-        }
-
-        //static async Task CopyingFilesFromOneDriveToTeams(string url, string accessToken, IEnumerable<string> files)
-        //{
-        //    using (var httpClient = new HttpClient())
-        //    {
-        //        var body = new StringContent(content, Encoding.UTF8, "application/json");
-
-        //        httpClient.DefaultRequestHeaders.Add("Authorization", $"bearer {accessToken}");
-
-        //        var response = await httpClient.PostAsync(url, body);
-        //        response.EnsureSuccessStatusCode();
-        //    }
-        //}
     }
 }
