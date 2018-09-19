d85265bfe4bf46c6ae0d76589a3880c02dc8594f
Justin Wendlandt <jwendl@hotmail.com>
2018-09-18 18:25:55 -0700

    Refactored configuration setup.

---
+++ src/LobAccelerator.Library/Configuration/ConfigurationManager.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+
+namespace LobAccelerator.Library.Configuration
+{
+    public class ConfigurationManager
+        : IConfiguration
+    {
+        public IConfiguration Configuration { get; }
+
+        public string this[string key]
+        {
+            get
+            {
+                var value = Configuration[key];
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentNullException(key);
+                return value;
+            }
+            set => throw new InvalidOperationException("You can't set this settings.");
+        }
+
+        public ConfigurationManager()
+        {
+            Configuration = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .Build();
+        }
+
+        public IConfigurationSection GetSection(string key)
+            => Configuration.GetSection(key);
+
+        public IEnumerable<IConfigurationSection> GetChildren()
+            => Configuration.GetChildren();
+
+        public IChangeToken GetReloadToken()
+            => Configuration.GetReloadToken();
+    }
+}
