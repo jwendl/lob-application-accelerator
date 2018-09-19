68ce0472c2da9046e940fa61e2f96b61e530d983
tessmichi <tess.distefano@gmail.com>
2018-09-19 15:37:51 -0700

    tests and fix storage integration

--- src/LobAccelerator.Library.Tests/StorageTests.cs
+++ src/LobAccelerator.Library.Tests/StorageTests.cs
@@ -1,6 +1,7 @@
 ﻿using LobAccelerator.Library.Tests.Utils.Configuration;
 using LobAccelerator.Library.Utils;
 using System;
+using System.Collections;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -31,6 +32,66 @@ namespace LobAccelerator.Library.Tests
 
             // Assert
             Assert.True(result);
+        }
+
+        [Fact]
+        public async Task UploadedBytesExistInBlob()
+        {
+            // Arrange
+            var bytes = new byte[5];
+            Random random = new Random();
+            random.NextBytes(bytes);
+
+            // Act
+            var uploaded = await StorageHelper.UploadBlobAsync(connectionString, containerName, blobName, bytes);
+            var exists = await StorageHelper.BlobExistsAsync(connectionString, containerName, blobName);
+
+            // Assert
+            Assert.True(uploaded);
+            Assert.True(exists);
+        }
+
+        [Fact]
+        public async Task UploadBytesExistBeforeNotAfterDeleteBlob()
+        {
+            // Arrange
+            var bytes = new byte[5];
+            Random random = new Random();
+            random.NextBytes(bytes);
+
+            // Act
+            var uploaded = await StorageHelper.UploadBlobAsync(connectionString, containerName, blobName, bytes);
+            var existsAfterUploaded = await StorageHelper.BlobExistsAsync(connectionString, containerName, blobName);
+            var removed = await StorageHelper.DeleteBlobAsync(connectionString, containerName, blobName);
+            var existsAfterRemoved = await StorageHelper.BlobExistsAsync(connectionString, containerName, blobName);
+
+            // Assert
+            Assert.True(uploaded);
+            Assert.True(existsAfterUploaded);
+            Assert.True(removed);
+            Assert.False(existsAfterRemoved);
+        }
+
+        [Fact]
+        public async Task DownloadBlobToBytes()
+        {
+            // Arrange
+            var bytes = new byte[5];
+            var downloadedBytes = new byte[5];
+            Random random = new Random();
+            random.NextBytes(bytes);
+
+            // Act
+            var uploaded = await StorageHelper.UploadBlobAsync(connectionString, containerName, blobName, bytes);
+            var exists = await StorageHelper.BlobExistsAsync(connectionString, containerName, blobName);
+            if (uploaded && exists)
+            {
+                downloadedBytes = await StorageHelper.DownloadBlobAsync(connectionString, containerName, blobName);
+            }
+
+            // Assert
+            Assert.True(uploaded);
+            Assert.True(((IStructuralEquatable)bytes).Equals(downloadedBytes, StructuralComparisons.StructuralEqualityComparer));
         }
     }
 }
