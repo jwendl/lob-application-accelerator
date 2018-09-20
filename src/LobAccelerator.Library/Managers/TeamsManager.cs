c232c6e12f12070eaa5f026a508cbca7b3d836ff
JuanK Ruiz <juanr@microsoft.com>
2018-09-20 16:12:15 -0700

    preparing to merge

--- src/LobAccelerator.Library/Managers/TeamsManager.cs
+++ src/LobAccelerator.Library/Managers/TeamsManager.cs
@@ -12,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using static LobAccelerator.Library.Extensions.ConstantsExtension;
 
