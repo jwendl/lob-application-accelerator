cab9bd174b935a0a940e67b1b0093aa62fb7bf78
Ian Bennett <ianbe@microsoft.com>
2018-09-20 18:15:49 -0400

    #48 Initial commit

--- src/LobAccelerator.Library/Managers/SharepointManager.cs
+++ src/LobAccelerator.Library/Managers/SharepointManager.cs
@@ -1,68 +1,64 @@
-﻿using LobAccelerator.Library.Interfaces;
+﻿using Microsoft.SharePoint.Client;
+using LobAccelerator.Library.Interfaces;
 using LobAccelerator.Library.Models.Common;
 using LobAccelerator.Library.Models.SharePoint;
 using LobAccelerator.Library.Models.SharePoint.Collections;
-using System;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
-using System.Xml.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Online.SharePoint.TenantAdministration;
 
 namespace LobAccelerator.Library.Managers
 {
     public class SharePointManager
         : ISharePointManager
     {
-        private readonly Uri baseUri;
+        private readonly IConfiguration configuration;
         private readonly HttpClient httpClient;
 
-        public SharePointManager(Uri baseUri, HttpClient httpClient)
+        public SharePointManager(IConfiguration configuration, HttpClient httpClient)
         {
-            this.baseUri = baseUri;
+            this.configuration = configuration;
             this.httpClient = httpClient;
         }
 
-        public async Task<IResult> CreateResourceAsync(SharePointResource sharePointResource)
+        public async Task<Result<SiteCollection>> CreateSiteCollectionAsync(SiteCollection siteCollection)
         {
-            var siteCollectionResult = await CreateSiteCollectionAsync(sharePointResource.DisplayName);
-
-            return Result.Combine(siteCollectionResult);
-        }
-
-        public async Task<Result<SiteCollection>> CreateSiteCollectionAsync(string title)
-        {
-            var requestUri = new Uri(baseUri, "/_api/Site/Collections");
-            var formDigest = await FetchFormDigestAsync(baseUri);
-            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, requestUri);
-            httpRequestMessage.Headers.Authorization = httpClient.DefaultRequestHeaders.Authorization;
-            httpRequestMessage.Headers.Add("X-RequestDigest", formDigest);
-
-            var requestContent = new StringContent("{ '__metadata': { 'type': 'SP.Data.AnnouncementsListItem' }, 'Title': '" + title + "'}");
-            requestContent.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json;odata=verbose");
-            httpRequestMessage.Content = requestContent;
-
-            var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
-            if (httpResponseMessage.IsSuccessStatusCode)
+            try
             {
-                var responseString = await httpResponseMessage.Content.ReadAsStringAsync();
+                var result = new Result<SiteCollection>();
+                var endpoint = $"https://{configuration["SharePointTenantPrefix"]}.sharepoint.com";
+                var context = new ClientContext(endpoint);
+                context.ExecutingWebRequest += Context_ExecutingWebRequest;
+                var tenant = new Tenant(context);
+                var webUrl = $"https://{configuration["SharePointTenantPrefix"]}.sharepoint.com/sites/rztest123";
+                var properties = new SiteCreationProperties()
+                {
+                    Url = siteCollection.Url,
+                    Owner = siteCollection.Owner,
+                    Title = siteCollection.Title,
+                    Template = siteCollection.Template,
+                    StorageMaximumLevel = siteCollection.StorageMaximumLevel,
+                    UserCodeMaximumLevel = siteCollection.UserCodeMaximumLevel
+                };
+                var op = tenant.CreateSite(properties);
+                context.Load(tenant);
+                context.Load(op, i => i.IsComplete);
+                context.ExecuteQuery();
+
+                result.Value = siteCollection;
+                return result;
             }
-            return null;
+            catch (System.Exception ex)
+            {
+                var x = ex;
+                throw ex;
+            }
         }
 
-        private async Task<string> FetchFormDigestAsync(Uri siteUri)
+        private static void Context_ExecutingWebRequest(object sender, WebRequestEventArgs e)
         {
-            //Get the form digest value in order to write data
-            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, new Uri(siteUri, "/_api/contextinfo"));
-            httpRequestMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
-            httpRequestMessage.Headers.Authorization = httpClient.DefaultRequestHeaders.Authorization;
-            var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
-
-            var responseString = await httpResponseMessage.Content.ReadAsStringAsync();
-            var xNamespace = "http://schemas.microsoft.com/ado/2007/08/dataservices";
-            var root = XElement.Parse(responseString);
-            var formDigestValue = root.Element(xNamespace + "FormDigestValue").Value;
-
-            return formDigestValue;
+            e.WebRequestExecutor.RequestHeaders.Add("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Imk2bEdrM0ZaenhSY1ViMkMzbkVRN3N5SEpsWSIsImtpZCI6Imk2bEdrM0ZaenhSY1ViMkMzbkVRN3N5SEpsWSJ9.eyJhdWQiOiJodHRwczovL3J6bmEtYWRtaW4uc2hhcmVwb2ludC5jb20iLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC83Nzk4YTBlZS0xODc1LTQ2MjQtOGRlOC03N2QyMWY4MTQwZDAvIiwiaWF0IjoxNTM3NDc0MjY2LCJuYmYiOjE1Mzc0NzQyNjYsImV4cCI6MTUzNzQ3ODE2NiwiYWNyIjoiMSIsImFpbyI6IjQyQmdZT2pnOTR2eGY3UXA2bGlDM0JLbHlqREhOOXd2Q21SeUVqSXFDLytIckcxYXNSWUEiLCJhbXIiOlsicHdkIl0sImFwcF9kaXNwbGF5bmFtZSI6IlNpdGVDb2xsZWN0aW9uQ3JlYXRlIiwiYXBwaWQiOiJmNzIyNDcxZi03NDY1LTQwZWEtOTE5YS04NjVlYzgwZmZmZDgiLCJhcHBpZGFjciI6IjEiLCJmYW1pbHlfbmFtZSI6ImRpWmVyZWdhIiwiZ2l2ZW5fbmFtZSI6IlJpY2hhcmQiLCJpcGFkZHIiOiI2OC4yMDMuMjcuMjMxIiwibmFtZSI6IlJpY2hhcmQgZGlaZXJlZ2EiLCJvaWQiOiJjY2FmNDhhMi0wZmUzLTRlMmQtODkxMy04MjljMGYxZGNmYWMiLCJwdWlkIjoiMTAwM0JGRkQ5Q0NDNDYyNCIsInNjcCI6IkFsbFNpdGVzLkZ1bGxDb250cm9sIiwic2lnbmluX3N0YXRlIjpbImttc2kiXSwic3ViIjoielBBamdrRk9EenNVaEZ3QUtBcDBkNUZZd24tMHlCcnZoTzFsM3lDamxVYyIsInRpZCI6Ijc3OThhMGVlLTE4NzUtNDYyNC04ZGU4LTc3ZDIxZjgxNDBkMCIsInVuaXF1ZV9uYW1lIjoicmljaEByaWNoZGl6ei5jb20iLCJ1cG4iOiJyaWNoQHJpY2hkaXp6LmNvbSIsInV0aSI6Im14Tk5Wd2pYUkVLSjRyUzRPZUVGQUEiLCJ2ZXIiOiIxLjAifQ.WAZvD1hxl5v3Di1CYA5TtTkk-_bxHOX6xkchdYpo6Gxgq3Ct_J-YvJgzKeeT66UK8kdTiswXYGSa8KWoZz2VrbsLd76xXa3CrECTokuam53pLgNsatvxEG1YjHQ0rgdxjBGBkz4N9txdwUFkqE9gp_mm9rON0PXUhckPMZInaZzuYP4nLCZyUIliMKOaZRVWJkWhn049zZ0PL8pQsDCWN_9s9GG7mHZE2LU--PEr0PIgcMWsN0FsuuDbHbJc7hOx2IISeWLC-t-W9DzgL9TSdUwkjWOoUuyx8GrjoXZlWfotcEvd5I368Apb2535xj-ZeKU1mcOCyaVKlzu6toNKcg");
         }
     }
 }
