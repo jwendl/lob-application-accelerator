964369510b57d8ddb6ac86d01afd968b6f35c097
Allan Targino <allan_targino@hotmail.com>
2018-09-20 19:48:08 -0300

    adding default nicknames

---
+++ src/LobAccelerator.Library/Utils/RandomHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LobAccelerator.Library.Utils
+{
+    static class RandomHelper
+    {
+        static readonly Random random = new Random();
+
+        public static string GetRandomString(int length)
+        {
+            return string.Concat(GetCharSequence(length));
+        }
+
+        private static IEnumerable<char> GetCharSequence(int length)
+        {
+            for (int i = 0; i < length; i++)
+                yield return (char) random.Next(97, 122);
+        }
+    }
+}
