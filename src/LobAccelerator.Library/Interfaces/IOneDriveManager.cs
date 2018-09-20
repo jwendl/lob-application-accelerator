0c93ab6d0e812a86ce4d72848b86382c423b2cfc
Allan Targino <allan_targino@hotmail.com>
2018-09-20 15:32:41 -0300

    supporting files into channels

--- src/LobAccelerator.Library/Interfaces/IOneDriveManager.cs
+++ src/LobAccelerator.Library/Interfaces/IOneDriveManager.cs
@@ -4,7 +4,7 @@ namespace LobAccelerator.Library.Interfaces
 {
     public interface IOneDriveManager
     {
-        Task CopyFileFromOneDriveToTeams(string teamId, string originOnedrivePath);
-        Task CopyFolderFromOneDriveToTeams(string teamId, string originOnedriveFolder);
+        Task CopyFileFromOneDriveToTeams(string teamId, string teamChannel, string originOnedrivePath);
+        Task CopyFolderFromOneDriveToTeams(string teamId, string teamChannel, string originOnedriveFolder);
     }
 }
