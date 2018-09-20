ab884a5f19c63b7c0b8fa379de01f452ee17eaa2
Fernando de Oliveira <fedeoliv@microsoft.com>
2018-09-20 20:54:04 +0000

    Configuration as data model

--- src/LobAccelerator.Client/Models/LobManager.cs
+++ src/LobAccelerator.Client/Models/LobManager.cs
@@ -9,24 +9,34 @@ namespace LobAccelerator.Client.Models
 {
     public class LobManager
     {
-        private readonly string _endpoint;
-        private readonly string _resource;
-        private readonly string _clientId;
+        private readonly Configuration _config;
         private readonly IEnumerable<string> _files;
 
         public LobManager(Options options)
         {
-            var configuration = new ConfigurationManager(options.ConfigurationFile);
-
-            _endpoint = configuration["LobEngine:Endpoint"];
-            _resource = configuration["AzureAd:Resource"];
-            _clientId = configuration["AzureAd:ClientId"];
+            _config = GetConfiguration(options.ConfigurationFile);
             _files = options.DefinitionsFiles;
+        }
+
+        private Configuration GetConfiguration(string configFile)
+        {
+            var configuration = new ConfigurationManager(configFile);
+
+            return new Configuration
+            {
+                Endpoint = configuration["LobEngine:Endpoint"],
+                AzureAd = new AzureAd
+                {
+                    Resource = configuration["AzureAd:Resource"],
+                    ClientId = configuration["AzureAd:ClientId"]
+                }
+            };
         }
 
         public async Task ProvisionResourcesAsync()
         {
-            var accessToken = await ConsoleExtensions.GetTokenByCode(_resource, _clientId);
+            var accessToken = await ConsoleExtensions
+                .GetTokenByCode(_config.AzureAd);
             
             using (var client = new HttpClient())
             {
@@ -40,7 +50,7 @@ namespace LobAccelerator.Client.Models
             var content = await file.GetFileContentAsync();
             var body = new StringContent(content, Encoding.UTF8, "application/json");
             
-            var response = await client.PostAsync(_endpoint, body);
+            var response = await client.PostAsync(_config.Endpoint, body);
             response.EnsureSuccessStatusCode();
         }
     }
