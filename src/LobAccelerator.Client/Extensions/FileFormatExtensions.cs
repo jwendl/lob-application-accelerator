f9fb63516ce12516f8a37e94b6b5388d789a2ec6
Fernando de Oliveira <fedeoliv@microsoft.com>
2018-09-20 18:22:56 +0000

    Yaml support for LOB Client

---
+++ src/LobAccelerator.Client/Extensions/FileFormatExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace LobAccelerator.Client.Extensions
+{
+    public static class FileFormatExtensions
+    {
+        public static bool IsYaml(this string fileExtension)
+        {
+            return fileExtension == ".yaml" || fileExtension == ".yml";
+        }
+    }
+}
